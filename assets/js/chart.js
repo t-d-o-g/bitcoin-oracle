96361f27e2d0b1c7956a6e92b1df1fdf77db01aa
Travis Collins <trc229@gmail.com>
2018-07-06 20:13:50 -0400

    Update header style and use canvasJS for graph

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,57 +1,45 @@
 window.onload = function() {
-
-
-
-
-
-
     var dataPoints = [];
+    var dates = [];
+    var bitcoinPrice = [];
     var startDate = '2018-06-25';
     var endDate = '2018-07-02';
-    var options =  {
-        animationEnabled: true,
-        theme: "light2",
-        title: {
-            text: "Bitcoin Historic Data"
-        },
-        axisX: {
-            valueFormatString: "DD MMM YYYY",
-        },
-        axisY: {
-            title: "USD",
-            titleFontSize: 24,
-            includeZero: false
-        },
-        data: [{
-            type: "spline", 
-            //yValueFormatString: "$#,###.##",
-            dataPoints: dataPoints
-        }]
-    };
-    
+
     $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", getPrice);    
+    $.getJSON("https://api.coindesk.com/v1/bpi/historical/close.json?start="+startDate+"&end="+endDate, addData);
     
     function getPrice(data) {
-        console.log(data.bpi.USD.rate_float);
        $("#pricedata").html(data.bpi.USD.rate_float);
     }
     
     function addData(data) {
-       // var dataArray= JSON.parse(data);
-        console.log(Object.getOwnPropertyNames(data.bpi).length);
-      
         for (var i = 0; i < Object.getOwnPropertyNames(data.bpi).length; i++) {
+            dates.push(Object.getOwnPropertyNames(data.bpi)[i]);
+            bitcoinPrice.push(data.bpi[Object.getOwnPropertyNames(data.bpi)[i]]);
             dataPoints.push({
                 x: new Date(Object.getOwnPropertyNames(data.bpi)[i]),
                 y: data.bpi[Object.getOwnPropertyNames(data.bpi)[i]]
             });
-            console.log(data.bpi[Object.getOwnPropertyNames(data.bpi)[i]]);
-            console.log(Object.getOwnPropertyNames(data.bpi)[i]);
         }
         
-        $("#chartContainer").CanvasJSChart(options);
-    
+        new Chart(document.getElementById("line-chart"), {
+            type: 'line',
+            data: {
+              labels: dates,
+            datasets: [{ 
+                data: bitcoinPrice,
+                label: "BitCoin",
+                borderColor: "#38fb38",
+                fill: false
+                }
+            ]
+            },
+            options: {
+            title: {
+                display: true,
+                text: 'BitCoin Historic Data'
+            }
+            }
+        });
     }
-    $.getJSON("https://api.coindesk.com/v1/bpi/historical/close.json?start="+startDate+"&end="+endDate, addData);
-    
-    }
+}
